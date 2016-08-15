bef9d9489a2d58d4cfd3d63d40d3f93cd62fe9ad
inna <NOfate281>
2016-08-15 16:48:34 +0300

    BONUS 1 +3 Done

--- src/task1.java
+++ src/task1.java
@@ -5,31 +5,16 @@ public class task1 {
 
     public static void main(String[] args){
 
-        Word();
-        Spaсe();
+        Word(); // Task 1
+        Spaсe(); // Task 2 + BONUS 3
+        WordWithM(); // BONUS 1
+
+        String text = "Lorem  ipsum dolor sit amet, consectetur " +
+                " adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua.";
 
     }
 
-    private static void Word() {
-
-        String text = "Lorem   ipsum dolor sit amet, consectetur " +
-                " adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua.";
-
-        boolean isContain = true;
-
-        while(isContain = text.contains("  ")) {
-
-            text = text.replace("  ", " ");
-        }
-
-            String[] textParts = text.split(" ");
-
-            System.out.println("Wolds amount = " + textParts.length);
-
-        }
-
-
-    private static void Spaсe() {
+        private static void Spaсe() {
 
             String text = "Lorem  ipsum dolor sit amet, consectetur " +
                     " adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua.";
@@ -38,14 +23,62 @@ public class task1 {
 
             int a = 0;
 
-                for(int i = 0; i<chArray.length; i++){
+            for(int i = 0; i<chArray.length; i++){
 
-                    if(chArray[i] == ' '){
+                if(chArray[i] == ' '){
 
-                        a = a + 1;
+                    a = a + 1;
+
                 }
             }
+
             System.out.println("Spaces amount = " + a);
+
         }
 
+        private static void Word() {
+
+            String text = "Lorem  ipsum dolor sit amet, consectetur " +
+                " adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua.";
+
+            boolean isContain = true;
+
+                while(isContain = text.contains("  ")) {
+
+                text = text.replace("  ", " ");
+
+            } // Bonus 3 Замінити подвійні пробіли одинарними
+
+            String[] textParts = text.split(" ");
+
+            System.out.println("Wolds amount = " + textParts.length);
+
+    }
+
+        private static void WordWithM() {
+
+            String text = "Lorem  ipsum dolor sit amet, consectetur " +
+                    " adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua.";
+
+            boolean isContain = true;
+
+                while(isContain = text.contains("  ")) {
+
+                text = text.replace("  ", " ");
+            }
+
+            String[] textParts = text.split(" ");
+
+            int WordWithM = 0;
+
+            for(int i = 0; i<textParts.length; i++){
+
+                if(textParts[i].contains("m")){
+
+                    WordWithM = WordWithM + 1;
+                }
+            }
+
+            System.out.println("Words with M amount = " + WordWithM);
+        }
     }
