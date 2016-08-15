7655068a93674c017556871ad286336386066690
inna <NOfate281>
2016-08-15 11:07:18 +0300

    test commit

--- src/task1.java
+++ src/task1.java
@@ -4,7 +4,12 @@
 public class task1 {
 
     public static void main(String[] args){
+        Spaсe();
 
-        
+    }
+        private static void Spaсe() {
+
+            String text = "Lorem  ipsum dolor sit amet, consectetur " +
+                    " adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore  magna aliqua.";
     }
 }
